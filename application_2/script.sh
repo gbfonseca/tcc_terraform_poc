1d1bc0f0f0f7d50eed0f7fdfb92ebb9a0fc85c6c
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-11-13 21:22:36 -0300

    feat: aplicação 2

--- application_2/script.sh
+++ application_2/script.sh
@@ -3,4 +3,4 @@ yum update -y
 yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
-echo '<h1>Bem-vindo a application_1 criada no projeto de TCC <h2>' >> /var/www/html/index.html
+echo '<h1>Bem-vindo a application_2 criada no projeto de TCC <h2>' >> /var/www/html/index.html
